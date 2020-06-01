9b4119d173f14a4794e5d5d14fa09fcaae4cf4ce
Nikita Sherstnev <sherstnyov@vivaldi.net>
2020-06-01 22:48:26 +0400

    Reluctant test deleted

--- lists/tests.py
+++ lists/tests.py
@@ -8,13 +8,9 @@ from lists.models import Item
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
-
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
